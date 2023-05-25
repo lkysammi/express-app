f9ffe875ac841056511645ec2f39ce566b15ba4b
lkysammi <kleun136@my.centennialcollege.ca>
2023-05-24 20:37:15 -0400

    created express-app

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware - express');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the another middleware - express');
+    res.send('<h1>This is my first application.</h1>');
+});
+
+app.listen(2000);
